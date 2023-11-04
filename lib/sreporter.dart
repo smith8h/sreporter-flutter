6fa64d752b1860c11cb265757cc34f522f704f07
Dev. Smith <husseinshakir81@gmail.com>
2023-11-04 14:50:11 +0300

    feat(init): set up tele service skeleton

--- lib/sreporter.dart
+++ lib/sreporter.dart
@@ -1,7 +1,17 @@
 library sreporter;
 
-class SReporter {
-  SReporter(var service);
+import 'tele_reporter.dart';
 
-  
+class SReporter {
+  static TeleService teleService({
+    required String botToken,
+    required String targetChat,
+    int targetChatTopic = 0,
+  }) {
+    return TeleService(
+      botToken: botToken,
+      targetChat: targetChat,
+      targetTopic: targetChatTopic,
+    );
+  }
 }
---
+++ lib/tele_reporter.dart
@@ -0,0 +1,34 @@
+// ignore_for_file: constant_identifier_names, prefer_final_fields, unused_field
+
+class TeleService {
+  static const int USER_INFO = 0;
+  static const int CUSTOM = 1;
+
+  final String botToken, targetChat;
+  int targetTopic;
+
+  String reportMessage = 'Message:\n',
+      reportHeader,
+      reportSubHeader = '',
+      reportFooter = 'More Information:',
+      deviceModel = 'Device model: ',
+      androidVersion = 'SDK version: ',
+      appVersion = '',
+      appName = 'App: ';
+
+  final String _noTokenMsg = 'NO BOT TOKEN PROVIDED!',
+      _noUsernameMsg = 'NO TARGET CHAT USERNAME/ID PROVIDED!',
+      _failMsg = 'REPORT SEND FAILED! CHECK YOUR INTERNET CONNECTION.',
+      _malformedUrl = 'MALFORMED URL! CHECK URL AGAIN.',
+      _noBodyMsg = 'NO REPORT MESSAGE PROVIDED BY THE USER!',
+      _noInternet = 'NO INTERNET CONNECTION!';
+
+  TeleService(
+      {required this.botToken,
+      required this.targetChat,
+      this.targetTopic = 0,
+      this.reportHeader = 'A New Report Sent from '});
+
+  // final Context context;
+  // PackageInfo info;
+}
